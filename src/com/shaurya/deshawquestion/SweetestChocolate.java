c50e2a2af0f175bf915a597f6763bcc0ffef58ea
Shaurya Manhar <smanhar747@gmail.com>
2020-07-09 17:19:01 +0530

    sweet chocolate solution using comparble interface

---
+++ src/com/shaurya/deshawquestion/SweetestChocolate.java
@@ -0,0 +1,85 @@
+package com.shaurya.deshawquestion;
+import java.util.*;
+public class SweetestChocolate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sweet[] = new int[n];
+        int bitter[] = new int[n];
+        String name[] = new String[n];
+        for(int i=0;i<n;i++){
+            sweet[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            bitter[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            name[i]=sc.next();
+        }
+        final int q =  sc.nextInt();
+        final int query[] = new int[q];
+        for(int i=0;i<q;i++){
+            query[i]=sc.nextInt();
+        }
+        //input end
+        solve(n, sweet, bitter, name, q, query);
+        sc.close();
+    }
+
+    static void solve(final int n,final int s[],final int b[],final String name[],final int q,final int query[]){
+        final ArrayList<Choco> list = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            list.add(new Choco(s[i], b[i], name[i]));
+        }
+        Collections.sort(list);
+        Collections.reverse(list);
+        // for(Choco c:list){
+        //     System.out.println(c.s+" "+c.b+" "+c.name);
+        // }
+        for(int i=0;i<q;i++){
+            System.out.print(list.get(query[i]-1).name+" ");
+        }
+    }
+    static class Choco implements Comparable<Choco>{
+        int s;int b;
+        String name;
+        public Choco(final int s,final int b,final String name){
+            this.s=s;
+            this.b=b;
+            this.name=name;
+        }
+
+        @Override
+        public int compareTo(final Choco o) {
+            if(s>o.s){
+                return 1;
+            }else if(s<o.s){
+                return -1;
+            }else{
+                if(b>o.b){
+                    return -1;
+                }else if(b<o.b){
+                    return 1;
+                }else{
+                    return -name.compareTo(o.name);
+                }
+            }
+        }
+    }
+}
+
+
+/*
+
+6
+2 3 7 3 2 2
+5 4 1 4 9 3
+a b c d e f
+6
+3 5 1 2 6 4
+
+output
+d a c b e f
+
+
+*/
