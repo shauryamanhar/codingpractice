683d7048f9fa7155521ddb9b2ab7b32376fa27ac
Shaurya Manhar <smanhar747@gmail.com>
2021-09-08 23:38:26 +0530

    shifting letters better approach

--- src/com/shaurya/leetcode/ShiftingLetters.java
+++ src/com/shaurya/leetcode/ShiftingLetters.java
@@ -5,30 +5,21 @@ public class ShiftingLetters {
 	public static void main(String[] args) {
 		ShiftingLetters o = new ShiftingLetters();
 		String s = "abc";
-		int shift[] = { 3, 5, 9 };
-//		int shift[] = { 16, 5, 9 };
+//		int shift[] = { 3, 5, 9 };
+		int shift[] = { 16, 5, 9 };
 		String ans = o.shiftingLetters(s, shift);
 		System.out.println(ans);
 	}
 
 	public String shiftingLetters(String s, int[] shifts) {
-		int len = s.length();
-		int sum[] = new int[len];
-		int prev = 0;
-		for (int i = len - 1; i >= 0; i--) {
-			sum[i] = shifts[i] + prev;
-			prev = sum[i];
-		}
-		char c[] = new char[len];
-		for (int i = 0; i < len; i++) {
-			char ch = s.charAt(i);
-			int next = ch + sum[i];
-			if (next > 'z') {
-				next = next % 26;
+		char[] c = new char[s.length()];
+		int sum = 0;
+		for (int i = s.length() - 1; i >= 0; i--) {
+			sum = (sum + shifts[i]) % 26;
+			c[i] = (char) (s.charAt(i) + sum);
+			if (c[i] > 'z') {
+				c[i] -= 26;
 			}
-			System.out.println("value of next " + next);
-			c[i] = (char) (next % 26 + 'a');
-			System.out.println("c[i] " + c[i]);
 		}
 		return new String(c);
 	}
